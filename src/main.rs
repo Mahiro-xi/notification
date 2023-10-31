39d9e9aa1b4097a46f0b700cbe708c7454a8da56
Mahiro-xi <j22268tm@edu.tuis.ac.jp>
2023-10-31 14:37:59 +0900

    changed time_clock method

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,37 @@
-use clock::clock;
 use notify_rust::*;
-mod clock;
+
+const TITLE: &str = "入退室リマインダー[beta]";
+const IN_TIME: [&str; 4] = ["10:55", "13:10", "14:50", "16:30"];
+const OUT_TIME: [&str; 4] = ["12:25", "14:40", "16:20", "17:30"];
 fn main() {
-    // 入退出処理リマインダー
-    let title = "入退出処理リマインダー";
-    let time = clock();
-    let body = time;
+    let mut click_status = false;
+    let body = "test";
 
-    notify_do(title, &body);
+    click_status = notify_do(&body);
+    println!("{}, {}", click_status, get_date());
 
 }
 
-fn notify_do(title: &str, body: &str) {
+fn get_date() -> String{
+    let date = chrono::Local::now();
+    let date_str = date.format("%Y-%m-%d %H:%M").to_string();
+    return date_str;
+}
+
+
+fn notify_do(body: &str) -> bool{
+    let mut status = false;
     Notification::new()
-        .summary(title)
+        .summary(TITLE)
         .body(body)
+        .action("click", "Ok")
+        .hint(Hint::Resident(true))
         .show()
-        .unwrap();
-}
-
-fn notify_timing(){
-    // 入室時間
-    
-}
+        .unwrap()
+        .wait_for_action(|action| match action {
+            "click" | _ => {
+                status = true;
+            }
+        });
+    return status;
+    }
