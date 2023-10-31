4a74e13cb199868f4c0bf87e6e66b3cb0f101420
Mahiro-xi <j22268tm@edu.tuis.ac.jp>
2023-10-31 16:10:59 +0900

    fix

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,36 @@ const IN_TIME: [&str; 4] = ["10:55", "13:10", "14:50", "16:30"];
 const OUT_TIME: [&str; 4] = ["12:25", "14:40", "16:20", "17:30"];
 fn main() {
     let mut click_status = false;
-    Notification::new().summary(TITLE).body("起動しました").show().unwrap();
+    let date = chrono::Local::now();
+    let date_str = date.format("%a").to_string();
+    if date_str != "Tue" {
+        Notification::new()
+            .summary(TITLE)
+            .body("本日の勤務はありません")
+            .show()
+            .unwrap();
+        std::process::exit(0);
+    }
     loop {
-        std::thread::sleep(std::time::Duration::from_secs(1));
+
+        std::thread::sleep(std::time::Duration::from_secs(2));
         let date = chrono::Local::now();
         let date_str = date.format("%H:%M").to_string();
-        if IN_TIME.contains(&&*date_str) {
+        
+        // Debug
+        println!("Debug: {}\nclick_status: {}\nin_time: {}\nout_time: {}", get_date(), click_status, IN_TIME.contains(&&*date_str), OUT_TIME.contains(&&*date_str));
+        println!("");
+
+        if click_status == true && !(IN_TIME.contains(&&*date_str) | OUT_TIME.contains(&&*date_str)){
+            click_status = false;
+        }
+
+        if IN_TIME.contains(&&*date_str) && click_status == false{
             let body = "入室時間です";
             click_status = notify_do(&body,&"入室");
             println!("{}, {}", click_status, get_date());
-        } else if OUT_TIME.contains(&&*date_str) {
+
+        } else if OUT_TIME.contains(&&*date_str) && click_status == false {
             let body = "退室時間です";
             click_status = notify_do(&body,&"退室");
             println!("{}, {}", click_status, get_date());
@@ -37,12 +57,14 @@ fn notify_do(body: &str,inout: &str) -> bool{
         .body(body)
         .action("click", inout)
         .hint(Hint::Resident(true))
+        .timeout(Timeout::Never)
         .show()
         .unwrap()
         .wait_for_action(|action| match action {
-            "click" | _ => {
+            "click"  => {
                 status = true;
-            }
+            },
+            &_ => todo!()
         });
     return status;
     }
